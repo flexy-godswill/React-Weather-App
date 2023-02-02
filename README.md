93dd4066621950fbf097bb5ba657ddf974d1d772
Parrot <parrot@localhost.localdomain>
2023-02-02 10:49:22 +0100

    Pushing My First React Project(React-Weather-App)For The First Time

---
+++ README.md
@@ -0,0 +1,8 @@
+# React-Weather-App
+# Author: https://github.com/flexy-godswill,
+# Version: 1.0.0,
+# Date: 1st February 2023
+
+This project was created with "create-react-app"
+
+Run this Project With "npm start"
---
+++ src/components/weather/Weather.js
@@ -0,0 +1,96 @@
+import {useState} from 'react';
+import "./Weather.scss";
+
+const Weather = () => {
+    let today = new Date()
+    let date = today.getFullYear() + "-" + today.getDate() + "-" + (today.getMonth() + 1);
+
+    const [input, setInput] = useState("");
+    const [weather,setWeather] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(true);
+    const [errorMsg, setErrorMsg] = useState("");
+
+    const [dateToday, setDateToday] = useState(date);
+
+    const api = {
+        url: "https://api.openweathermap.org/data/2.5/",
+        // Enter Your Api Url Above
+        key:"e98ad4a56a7d420eb700b64032c5f995",
+        // EnTeeer Your Api key Above
+    }
+
+    const iconURL = "https://openweathermap.org/img/w/"
+
+    const getInput = (e) => {
+        setInput(e.target.value)
+    }
+
+    const getWeatherData = (e) =>{
+        if (e.key === "Enter"  && input === "") {
+            setErrorMsg("Input Cannot Be Empty")
+            setError(true)
+        }
+        if (e.key === "Enter" && input !== "") {
+            setIsLoading(true)
+            setError(true)
+            console.log("it is working");
+            fetch(`${api.url}weather?q=${input}&units=metrics&APPID=${api.key}`)
+            .then((res) =>{
+                if (!res.ok){
+                    throw Error("Failed To Fetch Data")
+                }
+                return res.json();
+            })
+
+            .then((data) => {
+                console.log(data);
+                setWeather(data);
+                setInput("");
+                setError(false);
+                setIsLoading(false);
+            })
+            .catch((err) =>{
+                console.log(err.message);
+                setError(true)
+                setErrorMsg(err.message)
+                setIsLoading(false)
+            })
+        }
+    }
+
+  return (
+    <section className='--100vh --center-all'>
+        <div className='container weather --flex-center'>
+            <div className="weather-app --text-light">
+                <h1>React Weather App</h1>
+                <p>{dateToday}</p>
+                <div className="--form-control --my2">
+                    <input type="text" placeholder='Enter City Name' onChange={getInput} value= {input}  onKeyPress ={getWeatherData} />
+                </div>
+
+                {error ? (
+                    <p className={errorMsg !== "" ? "error" : ""}>{errorMsg}</p>
+                ) : (
+                    <div className="result --card --my2">
+                    <h2>{weather.name}, {weather.sys.country}</h2>
+
+                    <div className='icon'>
+                        <img src={iconURL + weather.weather[0].icon + ".png"} alt={weather.weather[0].main} />
+                    </div>
+
+                    <p>Temp:{Math.round(weather.main.temp)}°C</p>
+                    <p>Weather: {weather.weather[0].main}</p>
+                    <p>Temp Range: {Math.round(weather.main.temp_min)} °C / 
+                        {Math.round(weather.main.temp_max)}°C
+                    </p>
+                </div>
+                )}
+                {isLoading && <h3>Loading...</h3>}
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Weather;
